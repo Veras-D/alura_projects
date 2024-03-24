f068a371068bb8c170fad809885c60a7e018f940
Veras-D <dveras2310@gmail.com>
2024-03-24 03:52:35 -0300

    add ex06

---
+++ logica-js-projeto_inicial/exercicios/ex06.js
@@ -0,0 +1,5 @@
+let num1 = parseFloat(prompt('Digite o primeiro número: '));
+let num2 = parseFloat(prompt('Digite o segundo número: '));
+
+let resultado = num1 * num2;
+console.log(`O resultado da multiplicação é: ${resultado}`);
