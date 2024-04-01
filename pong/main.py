8a6ce4e2db8c818fafcddedf5bf6bc8aebad9d0f
Veras-D <dveras2310@gmail.com>
2024-04-01 05:54:59 -0300

    MPV finish

--- pong/main.py
+++ pong/main.py
@@ -5,7 +5,7 @@ import config
 pygame.init()
 
 window = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
-pygame.display.set_caption('Pong')
+pygame.display.set_caption('Pong Game')
 
 running = True
 
@@ -31,23 +31,30 @@ while running:
     # Colocar Biri-Biri como trilha  sonora para o jogo
     
     if  config.BOLL_POS_X <= 0 or config.BOLL_POS_X >= config.SCREEN_WIDTH - config.BOLL_WIDTH:
-        config.SPEED_X = -config.SPEED_X
+        config.BOLL_POS_X = config.SCREEN_WIDTH // 2 - config.BOLL_WIDTH // 2
+        config.BOLL_POS_Y = config.SCREEN_HEIGHT // 2 - config.BOLL_HEIGTH // 2
+        config.ACER = 6
+        config.SPEED_X, config.SPEED_Y = config.speed_fun()
         # Usar isso para fazer pontuação
         # Copiar esse trecho mudando da parede apara o player e o adversário e player
             # Usar essa parte também para aumentar a aceleração
             # Colocar som nessa parte?
-        
-
+    
     if  config.BOLL_POS_Y <= 0 or config.BOLL_POS_Y >= config.SCREEN_HEIGHT - config.BOLL_HEIGTH:
         config.SPEED_Y = -config.SPEED_Y
         
     if  config.ADVS_POS_Y <= 0 or config.ADVS_POS_Y >= config.SCREEN_HEIGHT - config.CHAR_HEIGTH:
         config.ADVS_SPEED = -config.ADVS_SPEED
     
+    
     window.fill(config.BLACK)
-    pygame.draw.rect(window, config.WHITE, (config.BOLL_POS_X, config.BOLL_POS_Y, config.BOLL_WIDTH, config.BOLL_HEIGTH))
-    pygame.draw.rect(window, config.WHITE, (config.PLAYER_POS_X, config.PLAYER_POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
-    pygame.draw.rect(window, config.WHITE, (config.ADVS_POS_X, config.ADVS_POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
+    ball = pygame.draw.rect(window, config.WHITE, (config.BOLL_POS_X, config.BOLL_POS_Y, config.BOLL_WIDTH, config.BOLL_HEIGTH))
+    player = pygame.draw.rect(window, config.WHITE, (config.PLAYER_POS_X, config.PLAYER_POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
+    adversary = pygame.draw.rect(window, config.WHITE, (config.ADVS_POS_X, config.ADVS_POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
+    
+    if  ball.colliderect(player) or  ball.colliderect(adversary):
+        config.SPEED_X = -config.SPEED_X
+        config.ACER += 1
     
     pygame.display.update()
     
