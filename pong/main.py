f46e8d2688d57058f98aa9e8821810cbb4485579
Veras-D <dveras2310@gmail.com>
2024-04-01 04:53:56 -0300

    adversary now is moving

--- pong/main.py
+++ pong/main.py
@@ -14,16 +14,30 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
-    config.POS_X += config.SPEED_X * 5
-    config.POS_Y += config.SPEED_Y * 5
+    config.BOLL_POS_X += config.SPEED_X * config.ACER
+    config.BOLL_POS_Y += config.SPEED_Y * config.ACER
     
-    if  config.POS_X <= 0 or config.POS_X >= config.SCREEN_WIDTH - config.CHAR_WIDTH:
+    config.ADVS_POS_Y += config.ADVS_SPEED
+    
+    # Colocar Biri-Biri como trilha  sonora para o jogo
+    
+    if  config.BOLL_POS_X <= 0 or config.BOLL_POS_X >= config.SCREEN_WIDTH - config.BOLL_WIDTH:
         config.SPEED_X = -config.SPEED_X
+        # Usar isso para fazer pontuação
+        # Copiar esse trecho mudando da parede apara o player e o adversário e player
+            # Usar essa parte também para aumentar a aceleração
+        
 
-    if  config.POS_Y <= 0 or config.POS_Y >= config.SCREEN_HEIGHT - config.CHAR_HEIGTH:
+    if  config.BOLL_POS_Y <= 0 or config.BOLL_POS_Y >= config.SCREEN_HEIGHT - config.BOLL_HEIGTH:
         config.SPEED_Y = -config.SPEED_Y
+        
+    if  config.ADVS_POS_Y <= 0 or config.ADVS_POS_Y >= config.SCREEN_HEIGHT - config.CHAR_HEIGTH:
+        config.ADVS_SPEED = -config.ADVS_SPEED
+    
     window.fill(config.BLACK)
-    pygame.draw.rect(window, config.WHITE, (config.POS_X, config.POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
+    pygame.draw.rect(window, config.WHITE, (config.BOLL_POS_X, config.BOLL_POS_Y, config.BOLL_WIDTH, config.BOLL_HEIGTH))
+    pygame.draw.rect(window, config.WHITE, (config.PLAYER_POS_X, config.PLAYER_POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
+    pygame.draw.rect(window, config.WHITE, (config.ADVS_POS_X, config.ADVS_POS_Y, config.CHAR_WIDTH, config.CHAR_HEIGTH))
     
     pygame.display.update()
     
